e707994962edc77b48ea7a5c476b174b6202aa87
Deepika-Raju <84725619+Deepika-Raju@users.noreply.github.com>
2021-05-29 19:39:12 +0530

    Add files via upload

---
+++ day_3.py
@@ -0,0 +1,9 @@
+'''num = int(input("enter the number of rows:"))
+for i in range (1, num+1):
+	print("* "*i)'''
+num = 6
+for i in range (num, num+1):
+	print("* "*i)
+for j in range (num, num+1):
+	print("* "*j)
+dgujmu
